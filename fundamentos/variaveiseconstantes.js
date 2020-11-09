bfb32591afd62ce0278a22af6bb7ef64ccbb58c5
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-09 00:00:53 -0300

    exercicio variaveis e constantes
    exercicio de variaveis e constantes em javascript

---
+++ fundamentos/variaveiseconstantes.js
@@ -0,0 +1,20 @@
+//declaração de uma variavel var, de nome 'a', sendo atribuida a ela o valor inteiro 3;
+var a = 3
+
+//criação de uma variavel elt (nova forma vinda do ECMA script 2015)
+let b = 4
+
+var a = 300
+ b = 40
+
+console.log(a, b)
+
+a = 3000
+b = 100
+
+console.log(a, b)
+
+//declaração de constante em javascript, pela declaração 'const'
+const c = 5
+//c = 50
+console.log(c)
