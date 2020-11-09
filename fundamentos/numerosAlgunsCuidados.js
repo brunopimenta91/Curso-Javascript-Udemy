d35fb9caaa543e44c9f6cce96ff5f3217e48dd2d
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-09 00:14:25 -0300

    exercicio sobre cuidados
    exercicio sobre cuidados em javascript

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,14 @@
+console.log(7 / 0)  //divisao por 0 retorna infinito, no tipo Infinity
+
+console.log('100' / 2)  //ocorre uma conversão de uma string em numero
+
+console.log('show' + 2)  //retorna como 'Not a Number' - NaN
+
+console.log(0.1 + 0.7)  //javascript apresenta imprecisoes em operacoes com ponto flutuante. Prestar atenção!!!
+
+//console.log(10.toString())  //isso dá erro, pois não é possivel estar convertendo numero pra string, usando diretamente o numero
+
+console.log((10.145334).toFixed(2)) //é possivel cfixar numeros decimais, usando parenteses e usando a funcao toFixed(2)
+
+
+
