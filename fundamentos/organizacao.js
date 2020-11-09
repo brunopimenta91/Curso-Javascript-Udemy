8ea5be50b1d9b10c5b4ea5c54f5c7197a3852baa
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-09 00:12:19 -0300

    exercicio organizacao
    exercicio organizacao em blocos e linhas, em javascript

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,13 @@
+console.log('linha de código')  //uma linha de código
+
+//blocos de código, delimitados por um par de chaves
+{
+
+    //é possivel existirem blocos de código, dentro de outro bloco de código
+    {
+        //sequencia de duas sentenças de código
+        console.log('sequencia de codigo')
+        console.log('olá')
+        console.log('mundo')
+    }
+}
