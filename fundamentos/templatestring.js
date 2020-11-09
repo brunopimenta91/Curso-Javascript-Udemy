7bccaca5785448879265398def0bad3004a931f2
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-09 00:07:13 -0300

    exercicio template string
    exercicio template string em javascript

---
+++ fundamentos/templatestring.js
@@ -0,0 +1,13 @@
+
+const nome = 'Rebeca '
+const concatenacao = 'olá' + nome + '!'
+const template = `
+    olá
+    ${nome}`
+
+console.log(concatenacao, template)
+
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = textto => textto.toUpperCase()
+console.log(`Ei... ${up('cuidadinho')}`)
