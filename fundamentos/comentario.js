11ce0a28bd6823895e76f09f6e869f59bfb48fca
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-08 23:57:21 -0300

    escrevi os comentarios
    Reescrevi os comentários, pois estavam errados.

--- fundamentos/comentario.js
+++ fundamentos/comentario.js
@@ -1,16 +1,14 @@
-//comentÃ¡rio de uma linha
+//comentário de uma linha
 console.log('linha 1')
 
 /*
-ComentÃ¡rio de
-mÃºltiplas 
+Comentário de multiplas 
 linhas!
 */
 console.log('linha 2')
 
 /*
-* ComentÃ¡rio
-* de vÃ¡rias
+* Comentário de multiplas
 * linhas, mas de outra forma
 */
 console.log('linha 3')
