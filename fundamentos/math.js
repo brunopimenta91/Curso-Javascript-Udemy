66b9cf568df60b4e6a9363da4eedd4217d00d2c1
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-09 00:18:54 -0300

    exercicio math
    exercicio sobre o metodo/funcao math em javascript

---
+++ fundamentos/math.js
@@ -0,0 +1,7 @@
+const raio = 5.6
+
+//Em javascripttem um objeto Math.PI (que retorna o valor de PI) e Math.pow(base, expoente) que faz uma exponenciação
+const area = Math.PI * Math.pow(raio, 2)
+
+console.log(area)
+console.log(typeof Math)  //tipo de Math é um objeto !!!!
