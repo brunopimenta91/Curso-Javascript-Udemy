a4fa107638a7e95a342a4f2bc5e8193fc0274b2d
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-08 23:55:06 -0300

    exercicio comentario
    exercicio sobre a aula de comentarios em javascript

---
+++ fundamentos/comentario.js
@@ -0,0 +1,16 @@
+//comentÃ¡rio de uma linha
+console.log('linha 1')
+
+/*
+ComentÃ¡rio de
+mÃºltiplas 
+linhas!
+*/
+console.log('linha 2')
+
+/*
+* ComentÃ¡rio
+* de vÃ¡rias
+* linhas, mas de outra forma
+*/
+console.log('linha 3')
