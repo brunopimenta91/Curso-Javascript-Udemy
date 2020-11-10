28fa1a55136032e210ff36505108e6b992c0d6b2
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-09 23:56:50 -0300

    exercicio objeto - parte 2
    exercicio objeto em javascript. Mostra agora dois atributos criados dinamicamente.

--- fundamentos/objeto.js
+++ fundamentos/objeto.js
@@ -2,4 +2,25 @@ const prod1 = {}  //declaração literal de um objeto vazio
 
 prod1.nome = 'celular ultra mega'  //₢riação de um atributo dinamicamente, no objeto vazio criado
 
+prod1.preco = 5000.50  //egundo atributo criado dinamicamente
+
+prod1['desconto legal'] = 0.40   //posivel de ser criado, MAS evite. Não é boa prática.
+
+
+//OBS: OS OBJETOS SÃO CONJUNTOS DE PARES CHAVE - VALOR
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
