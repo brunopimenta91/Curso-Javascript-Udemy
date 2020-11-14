928aec2796848954a0c3eb4bf17eee80946eea43
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-13 22:57:47 -0300

    exercicio atribuicao em javascript
    Exercicio mostrando como ocorre a atribuiçao de um valor a uma variavel (ou constante) ao msm tempo que uma operacao com os operadores aritmeticos fundamentais.

---
+++ fundamentos/atribucao.js
@@ -0,0 +1,40 @@
+//₢riada uma constante. Usamos um '=' para lhe atribuir um conteudo/valor à mesma.
+const a = 7
+
+let b = 3
+
+//atribuição aditiva  - atribui e ao mesmo tempo soma um valor a variavel do lado esquerdo
+b += a               //b = b + a
+console.log(b)
+
+//atribuicao subtrativa - atribui e ao mesmo tempo subtrai um valor a variavel do lado esquerdo
+b -= a              //b = b - a
+console.log(b)
+
+//atribuicao multiplicativa atribui e ao mesmo tempo multiplica um valor a variavel do lado esquerdo
+b *= 2               //b = b * 2
+console.log(b)
+
+//atribuicao divisiva atribui e ao mesmo tempo divide um valor a variavel do lado esquerdo
+b /=  2               //b = b / 2
+console.log(b)
+
+//atribuicao modular atribui o resto da divisao de um numero por outro, ao msm tendo que atribuindo o valor a variavel
+b %= 2                //b = b % 2
+console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
