bb88be3cd9d4291cb8c2b54ba56e23cdfe72e68a
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-15 23:12:00 -0300

    exercicio operadores aritmeticos em javascript
    Operações matematicas simples com os operadores aritmeticos em Javascript

---
+++ fundamentos/operadoresAritmeticos.js
@@ -0,0 +1,31 @@
+//operador de destruturação de quatros numeros de um array
+const [a, b, c, d] = [3, 5, 1, 9]
+
+const soma = a + b + c + d   //operacao aritmetica de soma sao sempre binarios (de dois operandos por vez) - sintaxe infix
+
+//operador unário postfix - vem depois do operando
+// d++
+
+//operador unário prefix - vem antes do operando
+//  -a
+
+//operação binaria - com dois operandos
+// a + d
+
+//operador aritmetica de subtração
+const subtracao = d - b
+
+//operador aritmetica de multiplicacao
+const multiplicacao = a * b
+
+//operador aritmetica de divisao 
+const divisao = d / a
+
+//operador aritmetica de modulo (valor resto da divisao entre dois operandos)
+const modulo = a % 2
+
+//imprimindo no console os valores
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
+
+
+
