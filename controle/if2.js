3896f02fddd456ccf2413eeafd6491b700cbfadc
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-18 00:40:04 -0300

    exercicio if 2 em jsavascript

---
+++ controle/if2.js
@@ -0,0 +1,29 @@
+
+function teste1(num) {
+    if(num > 7)
+        console.log(num)
+        
+    console.log('final')
+}
+
+//duas chamadas à função teste1
+teste1(8)
+teste1(6)
+
+
+
+//criada uma segunda funcao para testar o if
+function teste2(num) {
+    if(num >= 7) { //;  {   //CUIDADO COM O ';' - não usar COM AS ESTRUTURAS DE CONTROLE 
+        console.log(num)
+
+    }
+}
+
+//chamadas à função teste2
+teste2(5)
+teste2(9)
+
+
+
+
