1d32a59b263d438d3e1548edcf1022e6e9917f79
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-17 23:47:45 -0300

    Atualizando o exercicio if em javascript
    Incluindo uma segunda função, como exemplo do uso de 'if' em valores ditos true ou false em javascript.

--- controle/if1.js
+++ controle/if1.js
@@ -12,3 +12,15 @@ soBoaNoticia(8.1)
 soBoaNoticia(3.3)
 
 
+//₢riada uma segunda funcao, que rá fazer uso de um bloco condicional if - retornara se o valor dado como parametro para a funcao for um valor que em javascript, se comporte como true. Ja se for false, nada retornará
+function seForVerdadeEuFalo(valor) {
+    if(valor) {
+        console.log('é se comporta como verdadeiro o valor ' + valor)
+    }
+}
+
+
+
+
+
+
