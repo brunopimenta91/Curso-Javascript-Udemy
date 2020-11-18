a0e3b1e6c4182277be5241dfeb83bdb37353e4a1
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-17 23:40:04 -0300

    Explicacao bloco if em javascript

---
+++ controle/if1.js
@@ -0,0 +1,14 @@
+//₢riada uma funcao com um parametro nota
+
+function soBoaNoticia(nota) {
+    //bloco condicional if - se a relaçao no bloco ir for verdadeira, irá por em funcionamento os codigos entre os parenteses.
+    if(nota > 7) {
+        console.log('aprovado com ' + nota)
+    }
+}
+
+//Duas chamadas a funcao soBoaNoticia() - a primeira irá retorna uma frase e a segunda nada fará. 
+soBoaNoticia(8.1)
+soBoaNoticia(3.3)
+
+
