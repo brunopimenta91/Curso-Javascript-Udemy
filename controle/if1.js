61bc385f9dcdfe5c2b73656cc0d20e15ef12bf3f
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-18 00:27:22 -0300

    Atualizando o exercicio if em javascript
    Verificando quais tipos de valores em javascript, se comportam como true ou false, quando passados como parametros em uma funcao

--- controle/if1.js
+++ controle/if1.js
@@ -19,6 +19,28 @@ function seForVerdadeEuFalo(valor) {
     }
 }
 
+//chamadas diversas a funcao seForVerdadeEuFalo, com valores true ou false sendo passados como parametros
+seForVerdadeEuFalo()  //nenhum parametro passado - retorna nada
+seForVerdadeEuFalo(null) //parametro null 
+seForVerdadeEuFalo(undefined)  //orçando um parametro undefined 
+seForVerdadeEuFalo(NaN)  //NaN como parametro. tbm será false
+seForVerdadeEuFalo("")  //string sem nenhum espaço ocupado (nem que seja um espaço em branco) - nada retornará
+seForVerdadeEuFalo(0)   //0 é tratado como false tbm
+
+//os parametros que podem se comportar como verdadeiro
+seForVerdadeEuFalo(-1)   //nuemros diferentes de zero
+seForVerdadeEuFalo(' ')  //uma string de qualquer tamanho, inclsuive com um unico espaço em branco
+seForVerdadeEuFalo('?')   //string com um caractere usado só
+seForVerdadeEuFalo('teste') //tring de outro tamanho
+seForVerdadeEuFalo([])   //array vazio (sem elementos adicionados)
+seForVerdadeEuFalo([12, 3, 0.9])  //rray com elementos já presentes
+seForVerdadeEuFalo({})   //um objeto vazio como parametro
+seForVerdadeEuFalo({nome: 'ana', sobrenome: 'banana'})  //objeto com atributos e valores setados neles
+
+
+
+
+
 
 
 
