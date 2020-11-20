11d8d2c3739e5f7768cc118b58a02fa1ab08e25f
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-19 21:24:35 -0300

    exercicio do while em javascript
    exercicio sobre a estrutura de repetição DO WHILE, que garante ao menos uma interação do bloco de codigo dentro do laço DO WHILE, sendo que a expressao de controle vem APÓS o bloco de codigos.

---
+++ controle/dowhile.js
@@ -0,0 +1,18 @@
+
+function gerandoInteiroAleatorio(inicio, final) {
+    const valor = Math.random()  * (final - inicio) + inicio
+    return Math.floor(valor)
+}
+
+let numero = 0
+
+/*estrutura de repetição DO WHILE - pouco usada na prática em programas, mas viavel de ser usada. Grante ao menos 1 interação, independente se a relação dentro do while é TRUE ou FALSE*/
+do {
+    console.log(`numero gerado foi: ${numero}`)
+    numero = gerandoInteiroAleatorio(0, 10)
+} while (numero != 0)   //A ESTRUTURA DO WHILE É A unica estrutura de controle que o bloco de codigos executaveis vem ANTES da expressao dentro dos parantesese do while
+
+console.log('O while acabou!')
+
+
+
