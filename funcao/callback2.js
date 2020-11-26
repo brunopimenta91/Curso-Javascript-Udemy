f31c9a89df69bec9d90d5f42a5c1848670ed5feb
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-25 23:59:15 -0300

    Atualizando exercicio callback 2 javascript
    Mostrando abordagem de selecionar notas baixas de um array, SEM uso de callbacks.

--- funcao/callback2.js
+++ funcao/callback2.js
@@ -1,5 +1,14 @@
 //array criado para receber notas (valores numericos)
-const notas = [8.7, 8, 9.0, 2.45]
+const notas = [8.7, 8, 9.0, 2.45, 5.6, 10]
 
 
+//prnotasimeira abordagem - SEM CALLBACK
 
+let notasRuins = []
+for (let i in notas) {
+    if(notas[i] < 7) {
+        notasRuins.push(notas[i])
+    }
+}
+
+console.log(notasRuins)
