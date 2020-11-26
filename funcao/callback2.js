bd91bc3db290773070c4065700fbd575ae1716d1
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-26 00:24:14 -0300

    Atualizaçao final no exercicio callback 2 javascript
    Adicionando ao exercicio, alternativas com callbac, para selecionar valores presentes em um array em Javascript, coma  função interna filter()

--- funcao/callback2.js
+++ funcao/callback2.js
@@ -2,7 +2,7 @@
 const notas = [8.7, 8, 9.0, 2.45, 5.6, 10]
 
 
-//prnotasimeira abordagem - SEM CALLBACK
+//primeira abordagem - SEM CALLBACK
 
 let notasRuins = []
 for (let i in notas) {
@@ -12,3 +12,26 @@ for (let i in notas) {
 }
 
 console.log(notasRuins)
+
+
+
+/*segunda abordagem - COM CALLBACK - uma função anonima foi passada como parametro ds função interna do array criado, a filter()   */
+
+const notasRuins2 = notas.filter(function (nota) {
+    return nota < 7
+})
+
+console.log(notasRuins2)
+
+
+//callback com função arrow
+const notasRuins3 = notas.filter(nota => nota < 7)
+console.log(notasRuins3)
+
+//alternativa à arrow com uma constante auxiliar
+
+const notasMenorQue7 = nota => nota < 7
+const notasRuins4 = notas.filter(notasMenorQue7)
+console.log(notasRuins4)
+
+
