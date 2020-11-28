b13e744b2729083ecc3dd199ff1aafbade1ed014
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-27 21:42:48 -0300

    completando o exercicio factory 2 em Javscript
    Exercicio 2 sobre função factory, que mostra a declaração e uso prático de uma função factory em javascript. Função facotry é uma função cujo retorno é um objeto criado.

--- funcao/factory2.js
+++ funcao/factory2.js
@@ -1,4 +1,25 @@
-/*  função factory criada por mim, Bruno Pimenta 
+//função factory 2 - criada no exercicio pelo video da aula
+
+//função factory, com dois parametros passados: nome e preço
+function criarProduto(nome, preco) {
+    //retorno da função será um objeto, com 3 atributos: nome, preço e desconto.
+
+    return {
+        nome,         //nome: nome - meio alternativo de declarar e setar o atributo que recebe o valor passado no parametro
+        preco,        //por só preco é possivel, pois o nome do atributo é o mesmo que foi passado como parametro
+        desconto: 0.1    //atributo interno do objeto, com valor fixo de 0.1
+    }
+}
+
+
+//mostrando e ao mesmo tempo criando objetos, a partir da chamada da função factory criarProduto(parametro1, parametro2)
+
+//3 objetos criados, por meio da função factory (que cria objetos), de nome criarProduto()
+console.log(criarProduto('telefone fixo', 90))
+console.log(criarProduto('pilha D alcalina', 20,50))
+console.log(criarProduto('chocolate kitkat', 2))
+
+/*  função factory criada por mim, Bruno Pimenta
 
 function moldandoPessoa(nome, sobrenome) {
     return {
@@ -11,5 +32,5 @@ const pessoa1 = moldandoPessoa('bruno', 'moura')
 
 console.log(pessoa1)
 
-*/ 
+*/
 
