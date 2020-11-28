c47fd3b470a604aed72cbefb1d4860efc5de0a79
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-11-27 21:28:16 -0300

    exercicio sobre factory 2 em javascript
    Exercicio sobre função factory, com a função moldandoPessoa() criada por mim, Bruno, que retornará um objeto com os parametros passados por uma chamada da função.

---
+++ funcao/factory2.js
@@ -0,0 +1,15 @@
+/*  função factory criada por mim, Bruno Pimenta 
+
+function moldandoPessoa(nome, sobrenome) {
+    return {
+        nome: nome,
+        sobrenome: sobrenome
+    }
+}
+
+const pessoa1 = moldandoPessoa('bruno', 'moura')
+
+console.log(pessoa1)
+
+*/ 
+
