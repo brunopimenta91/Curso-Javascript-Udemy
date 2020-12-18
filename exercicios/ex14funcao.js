06a665b5c8c0d4a369fde5ef68b70817b556a9d0
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-12-18 01:02:04 -0300

    exercicio 14 sobre função em javascript
    Está incompleto. Ta dando erro.

---
+++ exercicios/ex14funcao.js
@@ -0,0 +1,27 @@
+/* 14) Crie uma estrutura condicional switch que receba uma string com o nome de uma fruta e que possua três
+casos: Caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com
+escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”. Teste com estas três opções .Crie
+também um default, que retornará uma mensagem de erro no console. */
+
+const pega_fruta = function (fruta) {
+    let frase 
+    
+    switch(fruta) {
+        case 'maça':
+            frase = 'Não vendemos esta fruta aqui'
+            break;
+        case 'kiwi':
+            frase = 'Estamos com escassez de kiwis'
+            break;
+        case 'melancia':
+            frase = 'Aqui está, são 3 reais o quilo'
+            break;
+        default:
+            frase = 'ERRO!!! mensagem de erro! FRUTA INVÁLIDA'
+            break;
+    }
+
+    return frase
+}
+
+console.log(pega_fruta(maça))
