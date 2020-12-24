1ea907e3c46e7483b88f9f554597d31faaacb83f
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-12-24 00:43:52 -0300

    exercicio 18 sobre funcao em javascript
    exercitiando a estrutura Switch.

---
+++ exercicios/ex18funcao.js
@@ -0,0 +1,49 @@
+/* Faça um programa que leia um número entre 0 e 10, e escreva este número por extenso. Use o comando
+switch. Crie um case default que escreva ‘Número fora do intervalo.’  */
+
+const lista_numeros = function (numero_digitado) {
+    let frase 
+
+    switch(numero_digitado) {
+        case 0:
+            frase = 'zero'
+            break;
+        case 1:
+            frase = 'um'
+            break;
+        case 2:
+            frase = 'dois'
+            break;
+        case 3:
+            frase = 'três'
+            break;
+        case 4:
+            frase = 'quatro'
+            break;
+        case 5:
+            frase = 'cinco'
+            break;
+        case 6:
+            frase = 'seis'
+            break;
+        case 7:
+            frase = 'sete'
+            break;
+        case 8:
+            frase = 'oito'
+            break;
+        case 9:
+            frase = 'nove'
+            break;
+        case 10:
+            frase = 'dez'
+            break;
+        default:
+            frase 'numero fora do intervalo entre 0 e 10'
+            break;
+    }
+
+    return frase
+}
+
+console.log(lista_numeros(3))
