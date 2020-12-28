96b9c213ef6babf0ff383d1f1ed2637595f1ad2a
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2020-12-27 23:49:53 -0300

    Atualização exercicio 19 sobre funcao em javascript

--- exercicios/ex19funcao.js
+++ exercicios/ex19funcao.js
@@ -11,12 +11,47 @@ a ser pago por aquele lanche. Considere que a cada execução somente será calc
 comando switch. Crie um caso default para produto não existente.   */
 
 function valor_lanche(lanche, quantidade) {
-    let valor_lanche
-    switch(lanche) \{
+    let preco_lanche
+    let total_lanche 
+    let nome_lanche
+
+    switch(lanche) {
         case 100:
             nome_lanche = 'Cachorro Quente'
-
+            preco_lanche = 3.00
+            break;
+        case 200:
+            nome_lanche = 'Hambúrguer Simples'
+            preco_lanche = 4.00
+            break;
+        case 300:
+            nome_lanche = 'Cheeseburguer'
+            preco_lanche = 5.50
+            break;
+        case 400:
+            nome_lanche = 'Bauru'
+            preco_lanche = 7.50
+            break;
+        case 500:
+            nome_lanche = 'Refrigerante'
+            preco_lanche = 3.50
+            break;
+        case 600:
+            nome_lanche = 'Suco'
+            preco_lanche = 2.80
+            break;
+        default:
+            console.log('erro! Lanche inexistente!')
+            break;
     }
+
+    total_lanche = preco_lanche * quantidade
+
+    return console.log(`${quantidade} unidades de ${nome_lanche} à R$ ${preco_lanche} cada um é um total de R$ ${total_lanche}!`)
 }
+
+valor_lanche(100, 10)
+
+valor_lanche(1000, 10)
 
 
