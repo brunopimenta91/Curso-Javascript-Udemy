ad7c65b3266f276b9a558f318624bd927e87972a
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2021-01-08 01:01:48 -0300

    exercicio 42 sobre funcao em javascript
    função que soma 2 numeros passados como parametros

---
+++ exercicios/ex42funcao.js
@@ -0,0 +1,17 @@
+//   Crie uma função que recebe 2 parâmetros e retorna a soma deles.
+
+//função com dois parametros
+function soma_de_2(numero1 = 0, numero2 = 0) {
+
+    return numero1 + numero2
+}
+
+
+//chamada diversas a função criada
+console.log(soma_de_2(100, 2))
+
+
+console.log(soma_de_2(455, 54))
+
+
+console.log(soma_de_2())
