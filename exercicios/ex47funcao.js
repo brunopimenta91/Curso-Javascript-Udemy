1f9f728cb5f9b0a997fba6e267ec78d0f68d5dd9
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2021-01-13 01:22:22 -0300

    exercicio 47 funcao em javascript

---
+++ exercicios/ex47funcao.js
@@ -0,0 +1,12 @@
+/* Escreva os números de 0 ~ 10  */
+
+const conta_ate10 = () => {
+
+    for(let conta = 1; conta <= 10; conta++) {
+        console.log(conta)
+    }
+}
+
+//chamada a função arrow (que não possui parametro) armazenada na constante conta_ate10, que irá exibir os inteiros positivos de 1 à 10
+
+conta_ate10()
