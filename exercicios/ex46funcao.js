42ba9ec9b750b52535d062e822080b6767df0820
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2021-01-13 01:03:31 -0300

    exercicio de funcao sobre javascript
    exercicio que verifica se com o ano de nascimento, a pessoa é menor ou maior de idade.

---
+++ exercicios/ex46funcao.js
@@ -0,0 +1,23 @@
+// Crie uma função que recebe o ano de nascimento da pessoa informando se ela é maior de idade ou menor.
+
+function maioridade_pessoa(ano_nascimento) {
+    let informacao
+
+    if((new Date().getFullYear() - ano_nascimento) < 18) {
+        informacao = 'menor de idade'
+    } else {
+        informacao = 'MAIOR de idade'
+    }
+
+    return informacao
+}
+
+//
+
+console.log(maioridade_pessoa(1991))
+
+console.log(maioridade_pessoa(2005))
+
+console.log(maioridade_pessoa(2002))
+
+console.log(maioridade_pessoa(2004))
