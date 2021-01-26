c681952a1209090f15a1dbc4de9c15eff3d78f81
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2021-01-26 20:56:58 -0300

    exercicio 1 - orientação a objeto
    Exercicio conceitual sobre a diferença entre o padrão de projetos de programação PROCEDURAL e o ORIENTADO A OBJETO.

---
+++ objeto/orientacao_objeto.js
@@ -0,0 +1,22 @@
+//CÓDIGO NÃO EXECUTÁVEL, SOMENTE PARA APRENDIZADO
+
+//procedural  - dados que são manipulados por funções/procedimentos
+processamento(valor1, valor2, valor)
+
+
+//orientação à objetos - dados com funções e atributos internos
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+
+    processamento() {
+        //corpo da função/procedimento
+    }
+}
+
+//conseguimos acessar os atributos e funções internas de objetos atraves da notação ponto '.' - ex:  objeto.valor1, objeto.processamento()
+objeto.processamento()
+objeto.valor1
+
+
