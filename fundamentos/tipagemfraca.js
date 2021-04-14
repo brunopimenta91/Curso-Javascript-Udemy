320547b94fdd1c96d5caa01b3ab9532714362fd3
Bruno Pimenta <68036257+brunopimenta91@users.noreply.github.com>
2021-04-14 00:37:51 -0300

    Mudando

--- fundamentos/tipagemfraca.js
+++ fundamentos/tipagemfraca.js
@@ -1,10 +1,12 @@
 let qualquer = 'legal'
 
-let nenhum = 'maldade'
-
 console.log(qualquer)
 console.log(typeof qualquer)
 
 qualquer = 3.1416
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = [12, 1.2, 0.4, 123.89]
 console.log(qualquer)
 console.log(typeof qualquer)
--- fundamentos/variaveiseconstantes.js
+++ fundamentos/variaveiseconstantes.js
@@ -1,7 +1,7 @@
 //declaração de uma variavel var, de nome 'a', sendo atribuida a ela o valor inteiro 3;
 var a = 3
 
-//criação de uma variavel elt (nova forma vinda do ECMA script 2015)
+//criação de uma variavel let (nova forma vinda do ECMA script 2015)
 let b = 4
 
 var a = 300
@@ -12,9 +12,12 @@ console.log(a, b)
 a = 3000
 b = 100
 
-console.log(a, b)
+console.log(a, b)  
+
+/*Como podemos ver na compilação do código, a variavel var a, assim como a variavel let b, podem ter seus valores 
+alterados. Ambos podem receber qualquer tipo de dados aceitos em Javascipt*/
 
 //declaração de constante em javascript, pela declaração 'const'
 const c = 5
-//c = 50
+ //c = 50   - Se por teste, quisessemos atribuir outro dado a constante c, o código apresentaria erro, pois uma variavel de tipo const tem seu valor imutável. IMPORTANTE!!
 console.log(c)
